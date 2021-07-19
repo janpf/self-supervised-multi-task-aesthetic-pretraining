4be5698f2f2892c2460d78b990c21dfcd05fd7bc
Jan Pfister <janpf@users.noreply.github.com>
2021-07-19 13:38:34 +0200

    Fixing #1

--- train_fine/test.py
+++ train_fine/test.py
@@ -8,9 +8,8 @@ import torch
 from torch import cuda
 
 sys.path[0] = "/workspace"
-from train_pre.dataset import SSPexelsSmall as SSPexels
+from train_fine.dataset import AVA
 from train_fine.NIMA import NIMA
-from train_pre.utils import mapping
 
 logging.basicConfig(format="%(asctime)s %(levelname)-8s %(message)s", level=logging.INFO, datefmt="%Y-%m-%d %H:%M:%S")
 # logging.getLogger("PIL.PngImagePlugin").setLevel(logging.INFO)
@@ -18,11 +17,14 @@ logging.basicConfig(format="%(asctime)s %(levelname)-8s %(message)s", level=logg
 parser = argparse.ArgumentParser()
 
 parser.add_argument("--model-path", type=str, default=None)
+
 config = parser.parse_args()
 
+config.model_path = str(config.model_path)
 
-test_file = "/workspace/dataset_processing/test_set.txt"
-out_file = "/workspace/analysis/not_uploaded/IA2NIMA/" + config.model_path.replace("/", ".") + ".txt"
+logging.info(f"loading {config.model_path}")
+
+out_file = "/workspace/analysis/not_uploaded/IA2NIMA/AVA/" + config.model_path.replace("/", ".") + ".txt"
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -34,40 +36,26 @@ nima.eval()
 
 logging.info("creating datasets")
 # datasets
-SSPexels_test = SSPexels(file_list_path=test_file, mapping=mapping, return_file_name=True)
-Pexels_test = torch.utils.data.DataLoader(SSPexels_test, batch_size=5, drop_last=False, num_workers=40)
+AVA = AVA(mode="test", horizontal_flip=False)
+AVA_test = torch.utils.data.DataLoader(AVA, batch_size=450, drop_last=False, num_workers=50, pin_memory=True)
 logging.info("datasets created")
 
 logging.info("testing")
 
 output = []
 
-for i, data in enumerate(Pexels_test):
-    logging.info(f"{i}/{len(Pexels_test)}")
+for i, data in enumerate(AVA_test):
+    logging.info(f"{i}/{len(AVA_test)}")
 
-    for key in data.keys():
-        if key == "file_name":
-            continue
+    with cuda.amp.autocast():
+        with torch.no_grad():
+            out = nima(data["img"].to(device))
 
-        img = data[key].to(device)
-        with cuda.amp.autocast():
-            with torch.no_grad():
-                out = nima(img)
-
-        for p, s in zip(data["file_name"], out.tolist()):
-            if key == "original":
-                key = "original;0"
-            if key == "rotate_original":
-                key = "rotate;0"
-            if key == "crop_original":
-                key = "crop;0"
-
-            out_dict = dict()
-            out_dict["img"] = p
-            out_dict["distortion"] = key.split(";")[0]
-            out_dict["level"] = key.split(";")[1]
-            out_dict["scores"] = s
-            output.append(out_dict)
+    for p, s in zip(data["path"], out.tolist()):
+        out_dict = dict()
+        out_dict["img"] = p
+        out_dict["scores"] = s
+        output.append(out_dict)
 
 logging.info(f"writing {out_file}")
 pd.DataFrame(output).to_csv(out_file, index=False)
